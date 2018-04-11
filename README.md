2d34765d16147cf0b412c2371c5e4615744fe37c
dr-matt-smith <dr_matt_smith@me.com>
2018-04-11 17:42:07 +0100

    added files to project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# -php-symfony4-book-codes-file-uploads-01-file-upload
+# php-symfony4-book-codes-file-uploads-01-file-upload
+
+based on Symfony file uploader example
+[https://symfony.com/doc/current/controller/upload_file.html](https://symfony.com/doc/current/controller/upload_file.html)
+
+and another one to solve Edit issue (and avoid having to re-upload each time:
+
+[https://ourcodeworld.com/articles/read/603/how-to-configure-a-file-uploader-for-a-single-field-doctrine-entity-in-symfony-3-3](https://ourcodeworld.com/articles/read/603/how-to-configure-a-file-uploader-for-a-single-field-doctrine-entity-in-symfony-3-3)
+
---
+++ src/Entity/Product.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @Assert\NotBlank(message="Please, upload the product brochure as a PDF file.")
+     * @Assert\File(mimeTypes={ "application/pdf" })
+     */
+    private $brochure;
+
+    public function getBrochure()
+    {
+        return $this->brochure;
+    }
+
+    public function setBrochure($brochure)
+    {
+        $this->brochure = $brochure;
+
+        return $this;
+    }
+}
